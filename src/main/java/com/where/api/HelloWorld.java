d7bdb9c5595b73bbbdd6fdc9ff5ab6daf98a9188
jeasong <cjs5241@gmail.com>
2023-12-25 18:33:43 +0900

    ecs ci/cd test

--- src/main/java/com/where/api/HelloWorld.java
+++ src/main/java/com/where/api/HelloWorld.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    @GetMapping("/hello")
+    @GetMapping("/health")
     public String test(){
         return "HelloWorld";
     }
