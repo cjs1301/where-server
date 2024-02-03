83d8d7bc40e708465313a6617357a81989ad9a27
jeasong <cjs5241@gmail.com>
2024-02-03 20:10:47 +0900

    채널 삭제 오류분 수정

--- src/main/java/com/where/api/domain/chating/repository/MessageRepository.java
+++ src/main/java/com/where/api/domain/chating/repository/MessageRepository.java
@@ -2,6 +2,9 @@ package com.where.api.domain.chating.repository;
 
 import com.where.api.domain.chating.entity.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +12,9 @@ import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<MessageEntity, UUID> {
     Optional<List<MessageEntity>> findAllByChannelId(UUID channel_id);
+
+    @Transactional
+    @Modifying
+    @Query("delete from MessageEntity m where m.channel.id = ?1")
+    void deleteAllByChannelId(UUID channel_id);
 }
