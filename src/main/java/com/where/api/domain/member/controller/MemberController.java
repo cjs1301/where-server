35bd18ff0db3240f1d2b64d0fc269212754eb836
jeasong <cjs5241@gmail.com>
2024-02-07 16:46:53 +0900

    url 오타 수정

--- src/main/java/com/where/api/domain/member/controller/MemberController.java
+++ src/main/java/com/where/api/domain/member/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
