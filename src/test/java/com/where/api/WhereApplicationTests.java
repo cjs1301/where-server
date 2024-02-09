331af8e6d73ca5223c051ad3621e102bbc1b92f6
jeasong <cjs5241@gmail.com>
2024-02-09 17:21:55 +0900

    kafka test 테스트용 임베디드 추가

--- src/test/java/com/where/api/WhereApplicationTests.java
+++ src/test/java/com/where/api/WhereApplicationTests.java
@@ -2,11 +2,17 @@ package com.where.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(properties = "spring.config.location=classpath:./application-test.yml")
 @ActiveProfiles(value = "test")
+@EmbeddedKafka(partitions = 3,
+        brokerProperties = {
+                "listeners=PLAINTEXT://localhost:9092"
+        },
+        ports = { 9092 })
 class WhereApplicationTests {
     @Test
     void contextLoads() {
