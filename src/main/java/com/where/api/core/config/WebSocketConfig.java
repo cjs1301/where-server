f97173d729aaeb0086b2911ca33422b8716ca611
jeasong <cjs5241@gmail.com>
2024-02-21 22:39:16 +0900

    rabbitmq test

--- src/main/java/com/where/api/core/config/WebSocketConfig.java
+++ src/main/java/com/where/api/core/config/WebSocketConfig.java
@@ -1,17 +1,24 @@
 package com.where.api.core.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 
 @Configuration
+@Slf4j
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${spring.rabbitmq.host}")
+    private String HOST;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        log.info(HOST);
         registry.setApplicationDestinationPrefixes("/pub")
                 .enableStompBrokerRelay("/topic")
+                .setRelayHost(HOST)
                 .setVirtualHost("/")
                 .setSystemHeartbeatReceiveInterval(10000);
     }
