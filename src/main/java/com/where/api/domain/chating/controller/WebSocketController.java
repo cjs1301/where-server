95094d9c894e4a87fd5205e4170eb2ce3c97b349
jeasong <cjs5241@gmail.com>
2024-02-21 23:17:24 +0900

    rabbitmq test3

--- src/main/java/com/where/api/domain/chating/controller/WebSocketController.java
+++ src/main/java/com/where/api/domain/chating/controller/WebSocketController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class WebSocketController {
-    private final SimpMessagingTemplate template;
+    private final SimpMessageSendingOperations template;
     private final ChannelService channelService;
 
     /*
