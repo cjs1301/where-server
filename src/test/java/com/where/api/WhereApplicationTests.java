fd6ce4003c75895721eb4dfa9426dcdbef1c145c
jeasong <cjs5241@gmail.com>
2024-02-21 18:44:40 +0900

    kafka 미삭제분 삭제

--- src/test/java/com/where/api/WhereApplicationTests.java
+++ src/test/java/com/where/api/WhereApplicationTests.java
@@ -2,7 +2,6 @@ package com.where.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
