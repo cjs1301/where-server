c178c4cb43f4488510a69c56b12013aabde23bef
jeasong <cjs5241@gmail.com>
2024-02-21 23:13:06 +0900

    rabbitmq test

--- src/main/java/com/where/api/core/config/WebSocketConfig.java
+++ src/main/java/com/where/api/core/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 
         registry.setApplicationDestinationPrefixes("/pub");
-        registry.enableStompBrokerRelay("/sub")
+        registry.enableStompBrokerRelay("/sub","/topic", "/queue")
                 .setRelayHost("rabbitmq")
                 .setClientLogin("guest")
                 .setClientPasscode("guest")
