11f7fa295c639a5ea322c06b65a3aa84040cbcbd
jeasong <cjs5241@gmail.com>
2024-02-21 21:03:37 +0900

    rabbitmq test

--- src/main/java/com/where/api/core/config/WebSocketConfig.java
+++ src/main/java/com/where/api/core/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .enableStompBrokerRelay("/topic")
                 .setRelayHost("rabbitmq")
                 .setVirtualHost("/")
-                .setRelayPort(5672)
+                .setRelayPort(61613)
                 .setClientLogin("guest")
                 .setClientPasscode("guest");
     }
