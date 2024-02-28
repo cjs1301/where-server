7a22d3e43fa0bafd9c2f9525f3872c25a5068f85
jeasong <cjs5241@gmail.com>
2024-02-28 16:53:04 +0900

    안드로이드 딥링크 확인

--- src/main/java/com/where/api/controller/DeepLinkController.java
+++ src/main/java/com/where/api/controller/DeepLinkController.java
@@ -10,11 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/.wll-known")
+@RequestMapping("/.well-known")
 public class DeepLinkController {
     @GetMapping("/apple-app-site-association")
     public Object iosDeeplinkHandler(){
         Resource resource = new ClassPathResource("apple-app-site-association.json");
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(resource.getInputStream(), Object.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @GetMapping("/assetlinks.json")
+    public Object androidDeeplinkHandler(){
+        Resource resource = new ClassPathResource("assetlinks.json");
         try {
             ObjectMapper mapper = new ObjectMapper();
             return mapper.readValue(resource.getInputStream(), Object.class);
