a348b1712d62db508d53a59662f4362f740beba8
cjs1301 <cjs5241@gmail.com>
2024-09-03 19:24:01 +0900

    test4

--- src/test/java/com/where/server/WhereApplicationTests.java
+++ src/test/java/com/where/server/WhereApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = "spring.config.location=classpath:./application-test.yml")
+@SpringBootTest
 @ActiveProfiles(value = "test")
 class WhereApplicationTests {
     @Test
