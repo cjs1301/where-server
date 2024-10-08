1d547dc304b2e37be5795620f6622d1e4e1a49ba
cjs1301 <cjs5241@gmail.com>
2024-10-08 22:29:17 +0900

    json 을 가져 오게 변경

--- module-api/src/main/java/org/where/moduleapi/api/controller/DeepLinkController.java
+++ module-api/src/main/java/org/where/moduleapi/api/controller/DeepLinkController.java
@@ -1,44 +1,39 @@
 package org.where.moduleapi.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/.well-known")
 public class DeepLinkController {
+    private final ObjectMapper objectMapper;
+    private final Resource appleAppSiteAssociation;
+    private final Resource assetLinks;
+
+    public DeepLinkController(ObjectMapper objectMapper,
+                              @Value("classpath:apple-app-site-association.json") Resource appleAppSiteAssociation,
+                              @Value("classpath:assetlinks.json") Resource assetLinks) {
+        this.objectMapper = objectMapper;
+        this.appleAppSiteAssociation = appleAppSiteAssociation;
+        this.assetLinks = assetLinks;
+    }
+
     @GetMapping("/apple-app-site-association")
-    public Map<String, Object> iosDeeplinkHandler() {
-        return Map.of(
-                "applinks", Map.of(
-                        "apps", List.of(),
-                        "details", List.of(
-                                Map.of(
-                                        "appID", "DD8NQWK3XA.com.app.where",
-                                        "paths", List.of("NOT /_/*", "/*")
-                                )
-                        )
-                )
-        );
+    public Map<String, Object> iosDeeplinkHandler() throws IOException {
+        return objectMapper.readValue(appleAppSiteAssociation.getInputStream(), Map.class);
     }
 
     @GetMapping("/assetlinks.json")
-    public List<Map<String, Object>> androidDeeplinkHandler() {
-        return List.of(
-                Map.of(
-                        "relation", List.of("delegate_permission/common.handle_all_urls"),
-                        "target", Map.of(
-                                "namespace", "android_app",
-                                "package_name", "com.app.odiya",
-                                "sha256_cert_fingerprints", List.of(
-                                        "1E:AD:1A:D3:B0:07:ED:2E:67:E0:BF:1B:D7:05:40:11:7F:1D:15:EC:38:78:38:BF:F6:FD:03:A2:11:68:56:87"
-                                )
-                        )
-                )
-        );
+    public List<Map<String, Object>> androidDeeplinkHandler() throws IOException {
+        return objectMapper.readValue(assetLinks.getInputStream(), List.class);
     }
 }
 
